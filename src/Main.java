c010a7f117dcecdb4e3f0ce867cd956af09775c7
beaks <drtanki781@gmail.com>
2023-04-28 20:53:37 +0400

    make it better

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import Database.*;
+import Database.Users.User;
+import GUI.Login.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.Window.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// Start up GUI, prompt to login
+		Login loginFrame = new Login();
+		loginFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		loginFrame.setVisible(true);
+	}
+}
