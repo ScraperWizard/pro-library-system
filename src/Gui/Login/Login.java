2e73e9155e169d373ede888ac007f479b1a638db
beaks <drtanki781@gmail.com>
2023-05-11 08:55:34 +0400

    Abdulrahman

--- src/Gui/Login/Login.java
+++ src/Gui/Login/Login.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.GroupLayout.Alignment;
 
 public class Login {
 	public JPanel contentPane;
@@ -21,18 +22,12 @@ public class Login {
 		Customers globalCustomersObject = new Customers();
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);
 		contentPane.setBackground(new Color(57, 130, 146));
 
 		// Get screen size
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int screenWidth = (screenSize.width) / 2;
 		int screenHeight = (screenSize.height) / 2;
-
-		JButton returnButton = new JButton("< Return");
-		returnButton.setForeground(new Color(255, 0, 0));
-		returnButton.setBounds(5, 5, 100, 20);
-		contentPane.add(returnButton);
 		
 		MouseAdapter panelMouseEventHandler = new MouseAdapter() {
 			@Override
@@ -56,98 +51,116 @@ public class Login {
 				event.getComponent().setCursor(Cursor.getDefaultCursor());
 			}
 		};
-
-		JPanel loginPanel = new JPanel();
-		loginPanel.setBounds(718, 262, 303, 300);
-		contentPane.add(loginPanel);
-		loginPanel.setBackground(new Color(57, 130, 146));
-		loginPanel.setLayout(new GridLayout(0, 1, 0, 0));
-
-		// Staff login heading
-		JLabel lblStaffHeading = new JLabel("Login as customer!");
-		lblStaffHeading.setHorizontalAlignment(SwingConstants.CENTER);
-		lblStaffHeading.setForeground(new Color(254, 255, 255));
-		loginPanel.add(lblStaffHeading);
-		lblStaffHeading.setFont(new Font("Dialog", Font.BOLD, 22));
-
-		// Username heading
-		JLabel lblUsernameHeading = new JLabel("Username:");
-		lblUsernameHeading.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
-		lblUsernameHeading.setForeground(new Color(254, 255, 255));
-		loginPanel.add(lblUsernameHeading);
-
-		// Username input
-		JTextField usernameInput = new JTextField();
-		usernameInput.setToolTipText("Username..");
-		loginPanel.add(usernameInput);
-
-		// Password heading
-		JLabel lblPasswordHeading = new JLabel("Password:");
-		lblPasswordHeading.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
-		lblPasswordHeading.setForeground(new Color(254, 255, 255));
-		loginPanel.add(lblPasswordHeading);
-
-		// Password input
-		JTextField passwordInput = new JPasswordField();
-		passwordInput.setToolTipText("Password...");
-		loginPanel.add(passwordInput);
-
-		// Login button
-		JButton customerLoginButton = new JButton("Login");
-		loginPanel.add(customerLoginButton);
-		customerLoginButton.setForeground(new Color(0, 52, 255));
-		
-		JLabel forgotPasswordLbl = new JLabel("Forgot your password?");
-		forgotPasswordLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
-		forgotPasswordLbl.setForeground(new Color(254, 255, 255));
-		forgotPasswordLbl.setHorizontalAlignment(SwingConstants.CENTER);
-		loginPanel.add(forgotPasswordLbl);
-				
-				JLabel orLbl = new JLabel("Or");
-				orLbl.setForeground(new Color(254, 255, 255));
-				orLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
-				orLbl.setHorizontalAlignment(SwingConstants.CENTER);
-				loginPanel.add(orLbl);
-				
-						JLabel registerNowLbl = new JLabel("Register");
-						registerNowLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
-						loginPanel.add(registerNowLbl);
-						registerNowLbl.setHorizontalAlignment(SwingConstants.CENTER);
-						registerNowLbl.setForeground(Color.WHITE);
-						registerNowLbl.addMouseListener(panelMouseEventHandler);
-
-		customerLoginButton.addActionListener(clickEvent -> {
-			// Get username and password
-			String username = usernameInput.getText();
-			String password = passwordInput.getText();
-			boolean validLogin = false;
-
-			// Validate the login
-			if (globalCustomersObject.validateLogin(username, password)) {
-				validLogin = true;
-				contentPane.setVisible(false);
-				// staffLogin staffLogin = new staffLogin();
-				// mainFrame.setContentPane(staffLogin.contentPane);
-			} else {
-				// Password error display
-				lblUsernameHeading.setText("Incorrect Username");
-				lblUsernameHeading.setForeground(Color.RED);
-				// Username error display
-				lblPasswordHeading.setText("Incorrect Password");
-				lblPasswordHeading.setForeground(Color.RED);
-				// Display login message
-				JOptionPane.showMessageDialog(null, validLogin ? "Valid login" : "Invalid login", "Login attempt",
-						JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-
-		returnButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent clickEvent) {
-				contentPane.setVisible(false);
-				loginSelector loginSelectorFrame = new loginSelector(mainFrame);
-				mainFrame.setContentPane(loginSelectorFrame.contentPane);
-			}
-		});
+						GridBagLayout gbl_contentPane = new GridBagLayout();
+						gbl_contentPane.columnWidths = new int[]{100, 613, 303, 0};
+						gbl_contentPane.rowHeights = new int[]{20, 237, 300, 0};
+						gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+						gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+						contentPane.setLayout(gbl_contentPane);
+								
+										JButton returnButton = new JButton("< Return");
+										returnButton.setForeground(new Color(255, 0, 0));
+										GridBagConstraints gbc_returnButton = new GridBagConstraints();
+										gbc_returnButton.fill = GridBagConstraints.BOTH;
+										gbc_returnButton.insets = new Insets(0, 0, 5, 5);
+										gbc_returnButton.gridx = 0;
+										gbc_returnButton.gridy = 0;
+										contentPane.add(returnButton, gbc_returnButton);
+										
+												returnButton.addActionListener(new ActionListener() {
+													public void actionPerformed(ActionEvent clickEvent) {
+														contentPane.setVisible(false);
+														loginSelector loginSelectorFrame = new loginSelector(mainFrame);
+														mainFrame.setContentPane(loginSelectorFrame.contentPane);
+													}
+												});
+						
+								JPanel loginPanel = new JPanel();
+								loginPanel.setBackground(new Color(57, 130, 146));
+								loginPanel.setLayout(new GridLayout(0, 1, 0, 0));
+								
+										// Staff login heading
+										JLabel lblStaffHeading = new JLabel("Login as customer!");
+										lblStaffHeading.setHorizontalAlignment(SwingConstants.CENTER);
+										lblStaffHeading.setForeground(new Color(254, 255, 255));
+										loginPanel.add(lblStaffHeading);
+										lblStaffHeading.setFont(new Font("Dialog", Font.BOLD, 22));
+										
+												// Username heading
+												JLabel lblUsernameHeading = new JLabel("Username:");
+												lblUsernameHeading.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+												lblUsernameHeading.setForeground(new Color(254, 255, 255));
+												loginPanel.add(lblUsernameHeading);
+												
+														// Username input
+														JTextField usernameInput = new JTextField();
+														usernameInput.setToolTipText("Username..");
+														loginPanel.add(usernameInput);
+														
+																// Password heading
+																JLabel lblPasswordHeading = new JLabel("Password:");
+																lblPasswordHeading.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+																lblPasswordHeading.setForeground(new Color(254, 255, 255));
+																loginPanel.add(lblPasswordHeading);
+																
+																		// Password input
+																		JTextField passwordInput = new JPasswordField();
+																		passwordInput.setToolTipText("Password...");
+																		loginPanel.add(passwordInput);
+																		
+																				// Login button
+																				JButton customerLoginButton = new JButton("Login");
+																				loginPanel.add(customerLoginButton);
+																				customerLoginButton.setForeground(new Color(0, 52, 255));
+																				
+																				JLabel forgotPasswordLbl = new JLabel("Forgot your password?");
+																				forgotPasswordLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+																				forgotPasswordLbl.setForeground(new Color(254, 255, 255));
+																				forgotPasswordLbl.setHorizontalAlignment(SwingConstants.CENTER);
+																				loginPanel.add(forgotPasswordLbl);
+																				
+																				JLabel orLbl = new JLabel("Or");
+																				orLbl.setForeground(new Color(254, 255, 255));
+																				orLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+																				orLbl.setHorizontalAlignment(SwingConstants.CENTER);
+																				loginPanel.add(orLbl);
+																				
+																						JLabel registerNowLbl = new JLabel("Register");
+																						registerNowLbl.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+																						loginPanel.add(registerNowLbl);
+																						registerNowLbl.setHorizontalAlignment(SwingConstants.CENTER);
+																						registerNowLbl.setForeground(Color.WHITE);
+																						GridBagConstraints gbc_loginPanel = new GridBagConstraints();
+																						gbc_loginPanel.fill = GridBagConstraints.BOTH;
+																						gbc_loginPanel.gridx = 2;
+																						gbc_loginPanel.gridy = 2;
+																						contentPane.add(loginPanel, gbc_loginPanel);
+																						registerNowLbl.addMouseListener(panelMouseEventHandler);
+																						
+																								customerLoginButton.addActionListener(clickEvent -> {
+																									// Get username and password
+																									String username = usernameInput.getText();
+																									String password = passwordInput.getText();
+																									boolean validLogin = false;
+																						
+																									// Validate the login
+																									if (globalCustomersObject.validateLogin(username, password)) {
+																										validLogin = true;
+																										contentPane.setVisible(false);
+																										// staffLogin staffLogin = new staffLogin();
+																										// mainFrame.setContentPane(staffLogin.contentPane);
+																									} else {
+																										// Password error display
+																										lblUsernameHeading.setText("Incorrect Username");
+																										lblUsernameHeading.setForeground(Color.RED);
+																										// Username error display
+																										lblPasswordHeading.setText("Incorrect Password");
+																										lblPasswordHeading.setForeground(Color.RED);
+																										// Display login message
+																										JOptionPane.showMessageDialog(null, validLogin ? "Valid login" : "Invalid login", "Login attempt",
+																												JOptionPane.INFORMATION_MESSAGE);
+																									}
+																								});
 
 	}
 }
--- src/Gui/loginSelector/loginSelector.java
+++ src/Gui/loginSelector/loginSelector.java
@@ -23,24 +23,32 @@ public class loginSelector {
         contentPane = new JPanel();
         contentPane.setBackground(new Color(57, 130, 146));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(null);
 
         // Get screen size
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int screenWidth = (screenSize.width) / 2;
         int screenHeight = (screenSize.height) / 2;
         
-        // Middle panel
-        JPanel loginSelectorPanel = new JPanel();
-        loginSelectorPanel.setBackground(new Color(57, 130, 146));
-        loginSelectorPanel.setForeground(new Color(57, 130, 146));
-        loginSelectorPanel.setBounds(718, 262, 303, 253);
-        
         LineBorder line = new LineBorder(new Color(87, 105, 120), 4, true);
-        loginSelectorPanel.setBorder(null);
-        contentPane.add(loginSelectorPanel);
-        loginSelectorPanel.setLayout(new GridLayout(0, 1, 0, 0));
-                
+        GridBagLayout gbl_contentPane = new GridBagLayout();
+        gbl_contentPane.columnWidths = new int[]{718, 303, 0, 0};
+        gbl_contentPane.rowHeights = new int[]{262, 253, 0, 0};
+        gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        contentPane.setLayout(gbl_contentPane);
+         
+         // Middle panel
+         JPanel loginSelectorPanel = new JPanel();
+         loginSelectorPanel.setBackground(new Color(57, 130, 146));
+         loginSelectorPanel.setForeground(new Color(57, 130, 146));
+         loginSelectorPanel.setBorder(null);
+         GridBagConstraints gbc_loginSelectorPanel = new GridBagConstraints();
+         gbc_loginSelectorPanel.fill = GridBagConstraints.BOTH;
+         gbc_loginSelectorPanel.gridx = 1;
+         gbc_loginSelectorPanel.gridy = 1;
+         contentPane.add(loginSelectorPanel, gbc_loginSelectorPanel);
+         loginSelectorPanel.setLayout(new GridLayout(0, 1, 0, 0));
+         
          // Staff login heading
          JLabel lblStaffHeading = new JLabel("Welcome to Pro Library!");
          lblStaffHeading.setHorizontalAlignment(SwingConstants.CENTER);
@@ -54,12 +62,12 @@ public class loginSelector {
          loginSelectorPanel.add(UserChoiceVar);
          UserChoiceVar.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
          UserChoiceVar.setHorizontalAlignment(SwingConstants.CENTER);
-                                
+         
          // Customer login button
          JButton customerLoginButton = new JButton("Customer");
          loginSelectorPanel.add(customerLoginButton);
          customerLoginButton.setForeground(new Color(0, 15, 255));
-                                        
+         
          // Customer event listener
          customerLoginButton.addActionListener(new ActionListener() {
         	 public void actionPerformed(ActionEvent clickEvent) {
@@ -68,7 +76,7 @@ public class loginSelector {
                  mainFrame.setContentPane(cusomterLoginFrame.contentPane);
              }
          });
-                        
+         
          // Staff login button
          JButton staffLoginButton = new JButton("Staff");
          loginSelectorPanel.add(staffLoginButton);
